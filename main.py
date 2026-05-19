270a62c5c803a4947f88674987b8f39222421cda
Anugrah N M <anugrah2011@gmail.com>
2026-05-19 20:01:00 +0100

    feat:Added id Query Parameter for GET

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 from dotenv import load_dotenv
 from datetime import date, datetime
 from pydantic import BaseModel, field_validator
+from typing import Optional
 
 load_dotenv()
 
@@ -18,7 +19,7 @@ app = FastAPI()
 # reminders = [{"name": "test", "due_date": "26/06/2026"}]
 
 @app.get("/reminders/")
-async def get_reminders():
+async def get_reminders(id: Optional[int] = None):
     try:    
         with psycopg2.connect(
             database=DB_NAME,
@@ -29,18 +30,20 @@ async def get_reminders():
             ) as connection_obj:     
             
             print("Connected to PostgreSQL")
+            cursor_obj = connection_obj.cursor()
+
+            if id is not None:
+                cursor_obj.execute("""SELECT * FROM reminders WHERE id = %s""", (id,))
+
+            else:
+                cursor_obj.execute("""SELECT * FROM reminders""")
+
+            rows = cursor_obj.fetchall()
+            return [{"id": row[0], "reminder_name": row[1], "due_date": str(row[2])} for row in rows]
+
     except psycopg2.Error as e:
         print(e)
-
-    cursor_obj = connection_obj.cursor()
-
-   
-
-    cursor_obj.execute("""SELECT * FROM reminders""")
-
-    rows = cursor_obj.fetchall()
-    return [{"id": row[0], "reminder_name": row[1], "due_date": str(row[2])} for row in rows]
-
+        return {"error": "Database issue", "details": str(e)}
 
 class Item(BaseModel):
     reminder_name: str
@@ -66,22 +69,23 @@ async def add_reminders(item: Item):
             ) as connection_obj:     
             
             print("Connected to PostgreSQL")
+            cursor_obj = connection_obj.cursor()
+
+            add_reminder_query = """
+                    INSERT INTO REMINDERS (reminder_name, due_date) VALUES (%s,%s)
+                """
+            ddate = item.due_date
+            
+            cursor_obj.execute(add_reminder_query, [item.reminder_name, ddate])
+            connection_obj.commit()
+            print("Reminder Added!")
+        
+            return item
+
     except psycopg2.Error as e:
         print(e)
-
-    cursor_obj = connection_obj.cursor()
-
-    add_reminder_query = """
-            INSERT INTO REMINDERS (reminder_name, due_date) VALUES (%s,%s)
-        """
-    ddate = item.due_date
+        return {"error": "Database issue", "details": str(e)}
     
-    cursor_obj.execute(add_reminder_query, [item.reminder_name, ddate])
-    connection_obj.commit()
-    print("Reminder Added!")
-   
-    return item
-
 
 @app.put("/reminders/{id}")
 async def edit_reminder(id: int, item: Item):
@@ -95,22 +99,26 @@ async def edit_reminder(id: int, item: Item):
             ) as connection_obj:     
             
             print("Connected to PostgreSQL")
+
+            cursor_obj = connection_obj.cursor()
+
+            
+            update_name_due_date_query = """
+                UPDATE REMINDERS
+                SET reminder_name = %s, due_date = %s
+                WHERE id = %s
+                """
+            cursor_obj.execute(update_name_due_date_query, [item.reminder_name, item.due_date, id])
+            connection_obj.commit()
+            print("Reminder Added!")
+
+            return id
+
     except psycopg2.Error as e:
         print(e)
+        return {"error": "Database issue", "details": str(e)}
 
-    cursor_obj = connection_obj.cursor()
 
-    
-    update_name_due_date_query = """
-        UPDATE REMINDERS
-        SET reminder_name = %s, due_date = %s
-        WHERE id = %s
-        """
-    cursor_obj.execute(update_name_due_date_query, [item.reminder_name, item.due_date, id])
-    connection_obj.commit()
-    print("Reminder Added!")
-
-    return id
 
 @app.delete("/reminders/{id}")
 async def delete_reminder(id: int):
@@ -124,18 +132,19 @@ async def delete_reminder(id: int):
             ) as connection_obj:     
             
             print("Connected to PostgreSQL")
-    except psycopg2.Error as e:
-        print(e)
 
-    cursor_obj = connection_obj.cursor()
-    
-    delete_reminder_query = """
+            cursor_obj = connection_obj.cursor()
+            
+            delete_reminder_query = """
                 DELETE FROM REMINDERS WHERE id = %s
             """
-    try:
-        cursor_obj.execute(delete_reminder_query, (id, ))
-        connection_obj.commit()
-    except ValueError as e:
+            try:
+                cursor_obj.execute(delete_reminder_query, (id, ))
+                connection_obj.commit()
+            except ValueError as e:
+                print(e)
+            
+            return id
+    except psycopg2.Error as e:
         print(e)
-    
-    return id
+        return {"error": "Database issue", "details": str(e)}
